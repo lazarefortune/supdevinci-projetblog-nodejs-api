bca4b110531339b1863bac798a0c05879013ad2b
Lazare Fortune <lazarefortune@gmail.com>
2022-03-17 10:53:41 +0100

    feat(routes): add users routes and swagger docs ref, without login route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,7 @@
 import User from "../db/models/User.js";
+import hashPassword from "../security/password/hashPassword.js";
 
 const usersRoutes = (app) => {
-  app.get("/users", async (req, res) => {
-    res.send("ALl users");
-  });
-
   /**
    * @swagger
    * /users:
@@ -20,6 +17,28 @@ const usersRoutes = (app) => {
     });
   });
 
+  /**
+   * @swagger
+   * /users/{id}:
+   *  get:
+   *    description: Use to request one user
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        value: 2
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    responses:
+   *      '200':
+   *          description: A successful response
+   *      '404':
+   *          description: User not found
+   *      '500':
+   *          description: Server error
+   */
   app.get("/users/:id", async (req, res) => {
     const { id: userId } = req.params;
 
@@ -31,6 +50,109 @@ const usersRoutes = (app) => {
       return;
     }
 
+    res.send(user);
+  });
+
+  app.post("/register", async (req, res) => {
+    const {
+      firstName,
+      lastName,
+      displayName,
+      email,
+      password,
+      createdAt,
+      updatedAt,
+    } = req.body;
+
+    const user = await User.query().findOne({ email });
+
+    if (user) {
+      res.status(409).send("User already exists");
+
+      return;
+    }
+
+    const [passwordHash, passwordSalt] = hashPassword(password);
+
+    User.query()
+      .insert({
+        firstName,
+        lastName,
+        displayName,
+        email,
+        createdAt,
+        updatedAt,
+        passwordHash,
+        passwordSalt,
+      })
+      .then((user) => {
+        res.send(user);
+      });
+  });
+
+  app.put("/users/:id", async (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, displayName, email, password, updatedAt } =
+      req.body;
+
+    const user = await User.query().findById(id);
+    if (!user) {
+      res.status(404).send("User not found");
+
+      return;
+    }
+
+    const [passwordHash, passwordSalt] = hashPassword(password);
+
+    await user
+      .$query()
+      .update({
+        firstName,
+        lastName,
+        displayName,
+        email,
+        passwordHash,
+        passwordSalt,
+        updatedAt,
+      })
+      .where("id", id);
+
+    res.send(user);
+  });
+
+  /**
+   * @swagger
+   * /users/{id}:
+   *  delete:
+   *    description: Use to delete one user
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        value: 12
+   *        schema:
+   *          type: integer
+   *        required: true
+   *    responses:
+   *      '200':
+   *          description: A successful response
+   *      '404':
+   *          description: User not found
+   *      '500':
+   *          description: Server error
+   */
+  app.delete("/users/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.query().findById(id);
+    if (!user) {
+      res.status(404).send("User not found");
+
+      return;
+    }
+
+    await user.$query().delete();
     res.send(user);
   });
 };
