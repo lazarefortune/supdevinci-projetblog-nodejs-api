d17760aecd91867d29829dc655bf2cd9fe85a235
Lazare Fortune <lazarefortune@gmail.com>
2022-03-22 20:33:53 +0100

    style(seeds): change roles  order priority,  set default admin at 1

--- src/db/seeds/02_roles.js
+++ src/db/seeds/02_roles.js
@@ -6,8 +6,8 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex("roles").del();
   await knex("roles").insert([
-    { name: "admin", orderPriority: 3 },
+    { name: "admin", orderPriority: 1 },
     { name: "author", orderPriority: 2 },
-    { name: "reader", orderPriority: 1 },
+    { name: "reader", orderPriority: 3 },
   ]);
 }
