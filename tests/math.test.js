76148faf67c7e5d60aed2689ef7d599cf5562ebe
Lazare Fortune <lazarefortune@gmail.com>
2022-03-25 11:45:49 +0100

    build(tests): add Jest lib for run all tests

---
+++ tests/math.test.js
@@ -0,0 +1,13 @@
+test("test somme math", () => {
+  expect(1 + 2).toBe(3);
+});
+
+describe("Users tests suites", () => {
+  test("test users", () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  test("test users 2", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
