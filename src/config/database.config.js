76e266f70be58620e8642ad586bbd4ca6c0d7e12
Lazare Fortune <lazarefortune@gmail.com>
2022-04-07 14:31:33 +0200

    style(database): edit database config structure

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,41 +2,50 @@ import "dotenv/config";
 
 const env = process.env;
 
-let connection = {};
+let dbConfig = {};
 
 if (env.NODE_ENV == "production") {
-  connection = {
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+  dbConfig = {
+    client: env.DB_CLIENT,
+    connection: {
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      user: env.DB_USER,
+      password: env.DB_PASSWORD,
+      database: env.DB_NAME,
+    },
   };
 }
 
 if (env.NODE_ENV == "development") {
-  connection = {
-    host: env.DB_HOST_DEV,
-    port: env.DB_PORT_DEV,
-    user: env.DB_USER_DEV,
-    password: env.DB_PASSWORD_DEV,
-    database: env.DB_NAME_DEV,
+  dbConfig = {
+    client: env.DB_CLIENT_DEV,
+    connection: {
+      host: env.DB_HOST_DEV,
+      port: env.DB_PORT_DEV,
+      user: env.DB_USER_DEV,
+      password: env.DB_PASSWORD_DEV,
+      database: env.DB_NAME_DEV,
+    },
   };
 }
 
 if (env == "test") {
-  connection = {
-    host: env.DB_HOST_TEST,
-    port: env.DB_PORT_TEST,
-    user: env.DB_USER_TEST,
-    password: env.DB_PASSWORD_TEST,
-    database: env.DB_NAME_TEST,
+  dbConfig = {
+    client: env.DB_CLIENT_TEST,
+    connection: {
+      host: env.DB_HOST_TEST,
+      port: env.DB_PORT_TEST,
+      user: env.DB_USER_TEST,
+      password: env.DB_PASSWORD_TEST,
+      database: env.DB_NAME_TEST,
+    },
   };
 }
 
 export default {
-  client: env.DB_CLIENT,
-  connection,
+  client: dbConfig.client,
+  connection: dbConfig.connection,
   migrations: {
     directory: "./src/db/migrations/",
   },
