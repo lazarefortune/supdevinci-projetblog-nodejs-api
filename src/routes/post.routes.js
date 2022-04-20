2b6c5db13185149ae77f7c8bc07c483c34d1d0e6
Lazare Fortune <lazarefortune@gmail.com>
2022-04-20 17:20:47 +0200

    style(routes): restyle routes

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -8,26 +8,30 @@ import {
   getAllPostComments,
   getAllPostsAsAdmin,
   getAllPostsWithoutAuth,
+  getPostWithoutAuth,
+  getAllPostCommentsWithoutAuth,
 } from "../controllers/post.controller.js";
 import auth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/public", getAllPostsWithoutAuth);
+router.get("/public/:id", getPostWithoutAuth);
+router.get("/public/:id/comments", getAllPostCommentsWithoutAuth);
 
-router.use([auth]);
+// router.use([auth]);
 
-router.get("/admin", getAllPostsAsAdmin);
-router.get("/", getAllPosts);
+router.get("/admin", auth, getAllPostsAsAdmin);
+router.get("/", auth, getAllPosts);
 
-router.get("/:id", getPost);
-router.get("/:id/comments", getAllPostComments);
+router.get("/:id", auth, getPost);
+router.get("/:id/comments", auth, getAllPostComments);
 
-router.post("/", createPost);
+router.post("/", auth, createPost);
 
-router.put("/:id", updatedPost);
-router.patch("/:id", updatedPost);
+router.put("/:id", auth, updatedPost);
+router.patch("/:id", auth, updatedPost);
 
-router.delete("/:id", deletePost);
+router.delete("/:id", auth, deletePost);
 
 export default router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,34 +1,24 @@
 import express from "express";
-import {
-  getAllUsers,
-  getUser,
-  signInUser,
-  createUser,
-  deleteUser,
-  updateUser,
-  allUserPosts,
-  allUserComments,
-  updateUserPassword,
-} from "../controllers/user.controller.js";
+import * as userController from "../controllers/user.controller.js";
 import auth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/login", signInUser);
-router.post("/register", createUser);
+router.post("/login", userController.signInUser);
+router.post("/register", userController.createUser);
 
 // Auth routes
-router.use([auth]);
+// router.use([auth]);
 
-router.get("/", getAllUsers);
-router.get("/:id", getUser);
-router.put("/:id", updateUser);
-router.patch("/:id", updateUser);
-router.put("/:id/password/update", updateUserPassword);
-router.delete("/:id", deleteUser);
+router.get("/", auth, userController.getAllUsers);
+router.get("/:id", auth, userController.getUser);
+router.put("/:id", auth, userController.updateUser);
+router.patch("/:id", auth, userController.updateUser);
+router.put("/:id/password/update", auth, userController.updateUserPassword);
+router.delete("/:id", auth, userController.deleteUser);
 
 // Other routes
-router.get("/:id/posts", allUserPosts);
-router.get("/:id/comments", allUserComments);
+router.get("/:id/posts", auth, userController.allUserPosts);
+router.get("/:id/comments", auth, userController.allUserComments);
 
 export default router;
