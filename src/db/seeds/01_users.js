79dd2ddf6028ee4342e5c23ea7732e9dfe881e96
Lazare Fortune <lazarefortune@gmail.com>
2022-04-23 00:38:17 +0200

    build(database): add initial users

--- src/db/seeds/01_users.js
+++ src/db/seeds/01_users.js
@@ -2,6 +2,57 @@ import faker from "faker";
 import { hashPassword } from "../../security/password/index.js";
 import { getRandomRole } from "../../utils/tools.js";
 
+const makeUserPasswordHashAndSalt = (user) => {
+  const [passwordHash, passwordSalt] = hashPassword(user.password);
+
+  delete user.password;
+
+  return {
+    ...user,
+    passwordHash,
+    passwordSalt,
+  };
+};
+
+const createInitialUsers = () => {
+  let initialUsers = [
+    {
+      firstName: "AdminFirstName",
+      lastName: "AdminLastName",
+      displayName: "adminDisplayName",
+      email: "admin@gmail.com",
+      password: "admin",
+      role: "admin",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      firstName: "AuthorFirstName",
+      lastName: "AuthorLastName",
+      displayName: "authorDisplayName",
+      email: "author@gmail.com",
+      password: "author",
+      role: "author",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      firstName: "UserFirstName",
+      lastName: "UserLastName",
+      displayName: "userDisplayName",
+      email: "user@gmail.com",
+      password: "user",
+      role: "reader",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
+
+  const users = initialUsers.map((user) => makeUserPasswordHashAndSalt(user));
+
+  return users;
+};
+
 const createFakeUser = () => {
   const [passwordHash, passwordSalt] = hashPassword(faker.internet.password());
 
@@ -30,5 +81,8 @@ export async function seed(knex) {
   for (let i = 0; i < desiredUsers; i++) {
     fakeUsers.push(createFakeUser());
   }
-  await knex("users").insert(fakeUsers);
+
+  const users = [...createInitialUsers(), ...fakeUsers];
+
+  await knex("users").insert(users);
 }
