c77e4e3963254f0fe3e0abc880ce3e8cfbf861f2
Lazare Fortune <lazarefortune@gmail.com>
2022-04-25 14:55:42 +0200

    style(middleware): add role middleware implementation

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,27 @@
+import AppError from "../utils/appError.js";
+
+const isGranted = (roles) => {
+  return (req, res, next) => {
+    try {
+      const {
+        session: {
+          user: { role },
+        },
+      } = req;
+
+      if (!roles.includes(role)) {
+        throw new AppError(
+          403,
+          "fail",
+          "You are not authorized to perform this action"
+        );
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default isGranted;
