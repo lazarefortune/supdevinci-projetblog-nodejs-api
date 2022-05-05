31f6630369c4dcf87de9ce5a47a721fb3a6d1524
Lazare Fortune <lazarefortune@gmail.com>
2022-05-05 14:43:27 +0200

    docs(app): add posts documentation without auth

--- docs/swagger.js
+++ docs/swagger.js
@@ -2,9 +2,8 @@ const swagger = {
   swagger: "2.0",
   info: {
     version: "1.0.0",
-    title: "API Documentation",
-    description:
-      "A simple API documentation for a Node.js and Express.js application about blog",
+    title: "Blog project API Documentation",
+    description: "A simple API documentation for this project",
     contact: {
       name: "Lazare Fortune",
     },
@@ -24,6 +23,236 @@ const swagger = {
   schemes: ["http"],
   consumes: ["application/json"],
   produces: ["application/json"],
+  paths: {
+    "/posts/admin": {
+      get: {
+        tags: ["Posts"],
+        summary: "Get all posts as Admin",
+        description: "Get all posts as Admin",
+        operationId: "getPostsAdmin",
+        produces: ["application/json"],
+        parameters: [],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              type: "array",
+              items: {
+                $ref: "#/definitions/Post",
+              },
+            },
+          },
+          500: {
+            description: "Internal server error",
+          },
+        },
+      },
+    },
+    "/posts": {
+      get: {
+        tags: ["Posts"],
+        summary: "Get all posts",
+        description: "Get all posts",
+        operationId: "getPosts",
+        produces: ["application/json"],
+        parameters: [],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              type: "array",
+              items: {
+                $ref: "#/definitions/Post",
+              },
+            },
+          },
+          500: {
+            description: "Internal server error",
+          },
+        },
+      },
+      post: {
+        tags: ["Posts"],
+        summary: "Create a new post",
+        description: "Create a new post",
+        operationId: "createPost",
+        produces: ["application/json"],
+        parameters: [
+          {
+            in: "content",
+            name: "content",
+            description: "Post object that needs to be added to the store",
+            required: true,
+            schema: {
+              $ref: "#/definitions/Post",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/Post",
+            },
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          404: {
+            description: "Post not found",
+          },
+          409: {
+            description: "Post already exists",
+          },
+          500: {
+            description: "Internal server error",
+          },
+        },
+      },
+    },
+    "/posts/{id}": {
+      get: {
+        tags: ["Posts"],
+        summary: "Get a single post",
+        description: "Get a single post",
+        operationId: "getPostById",
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID of post to return",
+            required: true,
+            type: "integer",
+            format: "int64",
+          },
+        ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/Post",
+            },
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          404: {
+            description: "Post not found",
+          },
+          500: {
+            description: "Internal server error",
+          },
+        },
+      },
+      put: {
+        tags: ["Posts"],
+        summary: "Update a post",
+        description: "Update a post",
+        operationId: "updatePost",
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID of post to return",
+            required: true,
+            type: "integer",
+            format: "int64",
+          },
+          {
+            in: "content",
+            name: "content",
+            description: "Post object that needs to be added to the store",
+            required: true,
+            schema: {
+              $ref: "#/definitions/Post",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/Post",
+            },
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          404: {
+            description: "Post not found",
+          },
+          500: {
+            description: "Internal server error",
+          },
+        },
+      },
+      delete: {
+        tags: ["Posts"],
+        summary: "Delete a post",
+        description: "Delete a post",
+        operationId: "deletePost",
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID of post to return",
+            required: true,
+            type: "integer",
+            format: "int64",
+          },
+        ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/Post",
+            },
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          404: {
+            description: "Post not found",
+          },
+          500: {
+            description: "Internal server error",
+          },
+        },
+      },
+    },
+  },
+  definitions: {
+    Post: {
+      type: "object",
+      properties: {
+        id: {
+          type: "integer",
+          format: "int64",
+        },
+        title: {
+          type: "string",
+        },
+        content: {
+          type: "string",
+        },
+        createdAt: {
+          type: "string",
+          format: "date-time",
+        },
+        updatedAt: {
+          type: "string",
+          format: "date-time",
+        },
+        authorId: {
+          type: "integer",
+          format: "int64",
+        },
+      },
+    },
+  },
 }
 
 export default swagger
