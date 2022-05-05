51ea6288b2c5a850816bffb05b37965d4db18057
Lazare Fortune <lazarefortune@gmail.com>
2022-05-05 20:42:28 +0200

    feat(app): change error response BREAKING CHANGE: error response become an object

--- src/controllers/error.controller.js
+++ src/controllers/error.controller.js
@@ -1,19 +1,21 @@
 import config from "../config/config.js"
 // eslint-disable-next-line no-unused-vars
 export default (err, req, res, next) => {
-  let errorDatas = {
-    statusCode: err.statusCode || 500,
-    message: err.message || "Something went wrong",
+  const errorDatas = {
+    error: {
+      statusCode: err.statusCode || 500,
+      message: err.message || "Something went wrong",
+    },
   }
 
   if (config.environment === "production" && errorDatas.statusCode === 500) {
-    errorDatas.message = "Internal Server Error"
+    errorDatas.error.message = "Internal Server Error"
   }
 
   if (config.environment === "development") {
     // If we're in development, we want to print the stack trace and other informations
-    errorDatas = { ...errorDatas, stack: err.stack }
+    errorDatas.error = { ...errorDatas.error, stack: err.stack }
   }
 
-  res.status(errorDatas.statusCode).json(errorDatas)
+  res.status(errorDatas.error.statusCode).json(errorDatas)
 }
